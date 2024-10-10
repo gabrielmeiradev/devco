a9fd03791e50bc8bc6f9d961c1c457cb23d780bd
gabrielmeiradev <gabriel_meira10@hotmail.com>
2024-10-10 17:46:24 -0300

    style: bio description weight

--- app/[username]/components/bio/index.tsx
+++ app/[username]/components/bio/index.tsx
@@ -3,7 +3,7 @@ export default function Bio({ bio }: { bio: string }) {
     return (
         <div className="p-8 bg-secondary/30 backdrop-blur-sm rounded-lg">
             <h1 className="text-base font-semibold text-muted-foreground mb-4">Bio</h1>
-            <p className="font-medium text-base">{bio}</p>
+            <p className="font-normal text-base">{bio}</p>
         </div>
     );
 }
