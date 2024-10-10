0a709a90bfd8cdc8bcc1b49f63e7ef10c9f28fdd
gabrielmeiradev <gabriel_meira10@hotmail.com>
2024-10-10 17:47:38 -0300

    style: skills font weight

--- app/[username]/components/profile-link/index.tsx
+++ app/[username]/components/profile-link/index.tsx
@@ -12,14 +12,14 @@ export default function ProfileLink({ label, icon: Icon, to, textColor }: IProfi
     return (
         <Link 
         href={to} 
-        className="
-            flex gap-2 items-center bg-white/5 
-            p-3 px-5 rounded-full font-medium 
-            text-base border border-transparent 
-            hover:bg-white/10 hover:border-white/5 
-            data-[state=open]:bg-white/10 data-[state=open]:border-white/5
-            focus:bg-white/10 focus:border-white/5
-            transition-colors outline-none 
+            className="
+                flex gap-2 items-center bg-white/5 
+                p-3 px-5 rounded-full font-normal 
+                text-base border border-transparent 
+                hover:bg-white/10 hover:border-white/5 
+                data-[state=open]:bg-white/10 data-[state=open]:border-white/5
+                focus:bg-white/10 focus:border-white/5
+                transition-colors outline-none 
         " 
         style={{ color: textColor }}>
             {Icon && <Icon className={"h-5"} />}
