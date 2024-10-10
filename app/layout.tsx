42a60a08bb642a3d95c5f9fe7be8e6f8b2f30c06
gabrielmeiradev <gabriel_meira10@hotmail.com>
2024-10-10 17:44:58 -0300

    changing font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,14 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from 'next/font/google'
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "</devco> | Seu portal dev",
   description: "Crie quantos portfólios quiser, para o que quiser e a qualquer hora",
 };
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
@@ -27,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} antialiased`}
       >
         <ThemeProvider
             attribute="class"
